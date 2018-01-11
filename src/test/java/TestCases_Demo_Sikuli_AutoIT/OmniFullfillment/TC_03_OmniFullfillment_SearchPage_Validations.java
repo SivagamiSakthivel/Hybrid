77b6a57e2fdbfdc9e2e5a49ffcdd4a86c0111194
Sivagami <sivagami.s20@gmail.com>
2018-01-11 10:05:12 +0530

    Extend Reports

--- src/test/java/TestCases_Demo_Sikuli_AutoIT/OmniFullfillment/TC_03_OmniFullfillment_SearchPage_Validations.java
+++ src/test/java/TestCases_Demo_Sikuli_AutoIT/OmniFullfillment/TC_03_OmniFullfillment_SearchPage_Validations.java
@@ -1,15 +1,18 @@
 package TestCases_Demo_Sikuli_AutoIT.OmniFullfillment;
 
+import ExtendReport.ExtendReportManager;
 import PageObjects.OmniFullfillmentSearchPage;
 import PageObjects.WorkLoadSummaryPage;
 import com.jacob.com.LibraryLoader;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
 
-public class TC_03_OmniFullfillment_SearchPage_Validations{
+public class TC_03_OmniFullfillment_SearchPage_Validations extends ExtendReportManager{
 
     OmniFullfillmentSearchPage search=new OmniFullfillmentSearchPage();
 
@@ -25,6 +28,11 @@ public class TC_03_OmniFullfillment_SearchPage_Validations{
 
         File file = new File("src\\main\\resources\\Lib", jacobDllVersionToUse);
         System.setProperty(LibraryLoader.JACOB_DLL_PATH, file.getAbsolutePath());
+
+        extent=new ExtentReports("D://MyExtendReport.html",true);
+        extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
+        logger=extent.startTest(("Validate Serach Page of OmniFullfillment"));
+       // logger=extent.startTest((this.getClass().getSimpleName()+"::"+method.getName()),method.getName());
     }
 
     @Test
@@ -35,10 +43,12 @@ public class TC_03_OmniFullfillment_SearchPage_Validations{
             search.waitTillPageLoad();
             search.LaunchOmniFullfillment();
             search.Validate_SearchPage_Contents();
+            logger.log(LogStatus.PASS,"Test Case Passed");
         }
         catch (Exception ex)
         {
             System.out.println(ex.getMessage());
+            logger.log(LogStatus.FAIL,"Test Case Failed");
         }
     }
     @AfterTest
